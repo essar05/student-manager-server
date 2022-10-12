1ed034452c778cc64842a0784573451f93236662
ropla <alexandru.platica@miele.com>
2022-10-12 18:52:56 +0300

    Add HTTPS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.CERT_KEY_FILE),
+    cert: fs.readFileSync(process.env.CERT_FILE),
+  };
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions,
+  });
   app.enableCors({
     origin: ['*'],
   });
